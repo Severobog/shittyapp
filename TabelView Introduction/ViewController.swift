38241c7a0c3d97baa5a61cfae4e4713a381e2193
Severobog <severobog@gmail.com>
2022-07-17 22:34:58 +0300

    some shitty commit

---
+++ TabelView Introduction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabelView Introduction
+//
+//  Created by Демид Стариков on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
